07f287943cb2a9673ea76eb1aff68eb3151e69aa
Nestor <nestor.colt@gmail.com>
2025-02-06 22:14:47 +0100

    scripted commit

---
+++ src/cognition/api.py
@@ -0,0 +1,12 @@
+from cognition_api.routes.agent import set_agent_handler
+from cognition_api.models import AgentRequest
+
+
+async def my_agent_handler(request: AgentRequest):
+    crew = Cognition().crew()
+    result = crew.kickoff(inputs=request.inputs)
+    return {"task": request.task, "result": result}
+
+
+# Register your handler
+set_agent_handler(my_agent_handler)
