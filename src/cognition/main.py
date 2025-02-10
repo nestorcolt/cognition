cf0ff985a1d558e6a3ce578e0e499b99f16838db
Nestor <nestor.colt@gmail.com>
2025-02-10 16:32:42 +0100

    scripted commit

--- src/cognition/main.py
+++ src/cognition/main.py
@@ -23,43 +23,43 @@ def run():
         raise Exception(f"An error occurred while running the crew: {e}")
 
 
-# def train():
-#     """
-#     Train the crew for a given number of iterations.
-#     """
-#     inputs = {"topic": "AI LLMs"}
-#     try:
-#         Cognition().crew().train(
-#             n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs
-#         )
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {"topic": "AI LLMs"}
+    try:
+        Cognition().crew().train(
+            n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs
+        )
 
-#     except Exception as e:
-#         raise Exception(f"An error occurred while training the crew: {e}")
-
-
-# def replay():
-#     """
-#     Replay the crew execution from a specific task.
-#     """
-#     try:
-#         Cognition().crew().replay(task_id=sys.argv[1])
-
-#     except Exception as e:
-#         raise Exception(f"An error occurred while replaying the crew: {e}")
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
 
 
-# def test():
-#     """
-#     Test the crew execution and returns the results.
-#     """
-#     inputs = {"topic": "AI LLMs"}
-#     try:
-#         Cognition().crew().test(
-#             n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs
-#         )
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        Cognition().crew().replay(task_id=sys.argv[1])
 
-#     except Exception as e:
-#         raise Exception(f"An error occurred while testing the crew: {e}")
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {"topic": "AI LLMs"}
+    try:
+        Cognition().crew().test(
+            n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs
+        )
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
 
 
 if __name__ == "__main__":
